a72e6d4d1d1b5fc03d6aa8725d47f4ce37ee9061
romanshrestha20 <stha.roman20@gmail.com>
2025-12-24 17:10:29 +0200

    feat: implement server setup with database connection and environment configuration

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from './app.js';
+import prisma from './lib/prismaClient.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT: number = parseInt(process.env.PORT || "3000", 10);
+// check database connection
+
+(async () => {
+    try {
+        await prisma.$connect();
+        console.log("Database connected");
+    } catch (err) {
+        console.error("Database connection error:", err);
+        process.exit(1);
+    }
+})();
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
