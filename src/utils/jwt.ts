d948e2a6925c297919855f82add874da6feb7f65
romanshrestha20 <stha.roman20@gmail.com>
2026-01-12 14:18:27 +0200

    feat: refactor JWT utility functions for improved structure and clarity

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,26 +1,35 @@
 import jwt from 'jsonwebtoken';
 import AppError from './appError.js';
 import 'dotenv/config';
+import { Verify } from 'node:crypto';
 
 
 
 export interface JwtPayload {
     userId: string;
 }
+// Access Token
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRY = '1h';
 
-const JWT_SECRET = process.env.JWT_SECRET
-if (!JWT_SECRET) {
-    throw new AppError("JWT_SECRET is not defined in environment variables", 500);
-}
+// Refresh Token
+const REFRESH_SECRET = process.env.REFRESH_SECRET;
+const REFRESH_EXPIRY = '7d';
+
+if (!JWT_SECRET) throw new AppError("JWT_SECRET is not defined in environment variables", 500);
+if (!REFRESH_SECRET) throw new AppError("REFRESH_SECRET is not defined in environment variables", 500);
 
 
-// Function to sign a JWT token with a payload
-export const signToken = (token: JwtPayload): string => {
-    return jwt.sign(token, JWT_SECRET as string, { expiresIn: '1h' });
+
+
+// Sign Access Token
+export const signAccessToken = (payload: JwtPayload): string => {
+    return jwt.sign(payload, JWT_SECRET as string, { expiresIn: JWT_EXPIRY });
 };
 
-// Function to verify a JWT token and return the decoded payload
-export const verifyToken = (token: string): JwtPayload => {
+
+// Verify Access Token
+export const verifyAccessToken = (token: string): JwtPayload => {
     try {
         const decoded = jwt.verify(token, JWT_SECRET as string) as JwtPayload;
         if (typeof decoded !== 'object' || !("userId" in decoded)) {
@@ -31,5 +40,27 @@ export const verifyToken = (token: string): JwtPayload => {
     } catch (error) {
         throw new AppError('Invalid or expired token', 401);
     }
+}
+
+
+// Sign Refresh Token
+export const signRefreshToken = (payload: JwtPayload): string => {
+    return jwt.sign(payload, REFRESH_SECRET as string, { expiresIn: REFRESH_EXPIRY });
 };
+
+// Verify Refresh Token
+export const verifyRefreshToken = (token: string): JwtPayload => {
+    try {
+        const decoded = jwt.verify(token, REFRESH_SECRET as string) as JwtPayload;
+        if (typeof decoded !== 'object' || !("userId" in decoded)) {
+            throw new AppError('Invalid token payload', 401);
+        }
+
+        return decoded as JwtPayload;
+    } catch (error) {
+        throw new AppError('Invalid or expired token', 401);
+    }
+}
+
+
 
