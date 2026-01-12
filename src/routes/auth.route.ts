ab71237575d0f22bdcc769cb35cc68adbf274b78
romanshrestha20 <stha.roman20@gmail.com>
2026-01-13 01:24:11 +0200

    feat: add missing routes for password management and refresh token

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,17 @@
 import express from "express";
-import { getUserProfile, login, logout, register, googleLogin, facebookLogin, changeUserPassword } from "../controllers/auth.controller.js";
+import {
+    getUserProfile,
+    login,
+    logout,
+    register,
+    googleLogin,
+    facebookLogin,
+    changeUserPassword,
+    refreshToken,
+    forgotPassword,
+    resetPassword,
+    verifyOtp
+} from "../controllers/auth.controller.js";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -7,9 +19,20 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+
+// OAuth routes
 router.post("/google", googleLogin);
 router.post("/facebook", facebookLogin);
+
+// Password management
 router.put("/change-password", authenticateUser, changeUserPassword);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp",  verifyOtp);
+router.post("/reset-password", resetPassword);
+
+// User profile
 router.get("/me", authenticateUser, getUserProfile);
 
+// Refresh token
+router.get("/refresh", refreshToken);
 export default router;
