52ffaca1f67be2a3ad5e6926ead50128b94d43c5
romanshrestha20 <stha.roman20@gmail.com>
2026-01-24 16:53:19 +0200

    refactor: improve code organization and logging in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,22 @@ import userRoutes from './routes/user.route.js';
 
 const app = express();
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-// Sample route
+
+app.use((req, _res, next) => {
+  console.log('Incoming request:', req.method, req.url);
+  next();
+});
+
+// Root test route
 app.get('/', (req, res) => {
     res.send('Hello, HelpAround Backend!');
 });
-
-
 
 export default app;
