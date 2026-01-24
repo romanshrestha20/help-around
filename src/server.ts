3a448398965a61680273402358e36c714c937df0
romanshrestha20 <stha.roman20@gmail.com>
2026-01-24 16:53:50 +0200

    refactor: update server startup logic and improve logging for database connection

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,22 @@ dotenv.config();
 import app from './app.js';
 import prisma from './lib/prismaClient.js';
 
-
-const PORT: number = parseInt(process.env.PORT || "3000", 10);
+const PORT: number = parseInt(process.env.PORT || '3004', 10);
 
 (async () => {
     try {
         await prisma.$connect();
         console.log("Database connected");
+
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port http://localhost:${PORT}`);
+        });
+
+
     } catch (err) {
         console.error("Database connection error:", err);
         process.exit(1);
     }
 })();
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+
